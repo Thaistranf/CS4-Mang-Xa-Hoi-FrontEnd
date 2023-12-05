c9e521bb3efa6743310abe4cc7afc0104fdd07c5
DUONGSON88 <songoku.20996@gmail.com>
2023-12-05 11:27:04 +0700

    font end last push

---
+++ js/home/blog.js
@@ -0,0 +1,105 @@
+function showBlog(){
+    document.getElementById("main").innerHTML=`
+
+    <header class="header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-4 col-md-3 order-2 order-sm-1">
+          <div class="header__social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+          </div>
+        </div>
+        <div class="col-sm-4 col-md-6 order-1  order-md-2 text-center">
+        <a href="" class="site-logo">
+        <h3>Moments</h3>
+        </a>
+        </div>
+        <div class="col-sm-4 col-md-3 order-3 order-sm-3">
+          <div class="header__switches">
+            <a href="#" class="search-switch"><i class="fa fa-search"></i></a>
+            <a href="#" class="nav-switch"><i class="fa fa-bars"></i></a>
+             <a href="" onclick="showFormLogin()"><i class="fa fa-user" aria-hidden="true">Login</i></a>
+          </div>
+        </div>
+      </div>
+      <nav class="main__menu">
+        <ul class="nav__menu">
+          <li><a href="" onclick="getAll()">Home</a></li>
+          <li><a href="javascript:" onclick="showProfile()">Profile</a></li>
+          <li><a href="javascript:" onclick="showGallery()">Gallery</a></li>
+          <li><a href="javascript:" onclick="showBlog()" class="menu--active">Blog</a></li>
+          <li><a href="javascript:" onclick="">Topics</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <!-- Header Section end -->
+
+  <!-- Blog Page -->
+  <section class="blog__page">
+    <div class="blog__warp">
+      <div class="row blog__grid text-white">
+        <div class="col-lg-8 col-xl-9">
+          <div class="row">
+            <div class="col-md-8 col-lg-7 col-xl-8">
+              <div class="blog__item set-bg" data-setbg="img/blog/1.jpg">
+                <div class="blog__content">
+                  <div class="blog__date">DEC 18, 2019</div>
+                  <h3><a href="./blog-single.html">9 Reasons to Buy a 50mm Prime Lens & Skip the Kit Lens</a></h3>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-lg-5 col-xl-4">
+              <div class="blog__item set-bg" data-setbg="img/blog/2.jpg">
+                <div class="blog__content">
+                  <div class="blog__date">DEC 18, 2019</div>
+                  <h4><a href="./blog-single.html">Assorted Textures FREE Stock Photos</a></h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-lg-5 col-xl-4">
+              <div class="blog__item set-bg" data-setbg="img/blog/4.jpg">
+                <div class="blog__content">
+                  <div class="blog__date">DEC 18, 2019</div>
+                  <h4><a href="./blog-single.html">Assorted Textures FREE Stock Photos</a></h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8 col-lg-7 col-xl-8">
+              <div class="blog__item set-bg" data-setbg="img/blog/5.jpg">
+                <div class="blog__content">
+                  <div class="blog__date">DEC 18, 2019</div>
+                  <h3><a href="./blog-single.html">Improve Your Portrait Photography with These Helpful Tips</a></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-xl-3">
+          <div class="blog__item blog__item--long set-bg" data-setbg="img/blog/3.jpg">
+            <div class="blog__content">
+              <div class="blog__date">DEC 18, 2019</div>
+              <h4><a href="./blog-single.html">Assorted Textures FREE Stock Photos</a></h4>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  <!-- Blog Page end -->
+
+  <!-- Search Begin -->
+  <div class="search-model">
+    <div class="h-100 d-flex align-items-center justify-content-center">
+      <div class="search-close-switch">+</div>
+      <form class="search-model-form">
+        <input type="text" id="search-input" placeholder="Search here.....">
+      </form>
+    </div>
+  </div>
+  <!-- Search End -->
+    `
+}
---
+++ js/home/profile.js
@@ -0,0 +1,117 @@
+function showProfile(){
+    document.getElementById("main").innerHTML=`
+
+    <header class="header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-4 col-md-3 order-2 order-sm-1">
+          <div class="header__social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+          </div>
+        </div>
+        <div class="col-sm-4 col-md-6 order-1  order-md-2 text-center">
+                    <a href="" class="site-logo">
+                        <h3>Moments</h3>
+                    </a>
+        </div>
+        <div class="col-sm-4 col-md-3 order-3 order-sm-3">
+          <div class="header__switches">
+            <a href="#" class="search-switch"><i class="fa fa-search"></i></a>
+            <a href="#" class="nav-switch"><i class="fa fa-bars"></i></a>
+             <a href="" onclick="showFormLogin()"><i class="fa fa-user" aria-hidden="true">Login</i></a>
+          </div>
+        </div>
+      </div>
+      <nav class="main__menu">
+        <ul class="nav__menu">
+          <li><a href="" onclick="getAll()">Home</a></li>
+          <li><a href="javascript:" onclick="showProfile()" class="menu--active">Profile</a></li>
+          <li><a href="javascript:" onclick="showGallery()">Gallery</a></li>
+          <li><a href="javascript:" onclick="showBlog()">Blog</a></li>
+          <li><a href="javascript:" onclick="">Topics</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <!-- Header Section end -->
+
+  <!-- About Page -->
+  <section class="about__page">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="about__text">
+            <h3 class="about__title">Profile Me</h3>
+            <div class="about__meta">
+              <img src="/img/profile.jpg" alt="">
+              <div class="about__meta__info">
+                <h5>Caleb Rodriguez</h5>
+                <p>PHOTOGRAPHER / DESIGNER</p>
+              </div>
+            </div>
+            <p>Date Of Birth</p>
+            <p>Email</p>
+            <br>
+            <a href=""><i class="fa fa-cog" aria-hidden="true"></i>Change Profile </a>
+            <br>
+            <img src="/img/signature.png" alt="">
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="experience__text">
+            <h3 class="about__title">My Album</h3>
+            <div class="experience__item">
+              <div>Album1</div>
+              
+            </div>
+            <div class="experience__item">
+              
+              <div>Album2</div>
+            </div>
+            <div class="experience__item">
+              
+              <div>Album3</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="skills__text">
+            <h3 class="about__title">Skills</h3>
+            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Vivamus at nibh tincidunt, bibendum ligula id. </p>
+            <div class="single-progress-item">
+              <h6>Development</h6>
+              <div class="progress-bar-style" data-progress="70"></div>
+            </div>
+            <div class="single-progress-item">
+              <h6>APP Design</h6>
+              <div class="progress-bar-style" data-progress="70"></div>
+            </div>
+            <div class="single-progress-item">
+              <h6>Graphic Design</h6>
+              <div class="progress-bar-style" data-progress="70"></div>
+            </div>
+            <div class="single-progress-item">
+              <h6>Photography</h6>
+              <div class="progress-bar-style" data-progress="70"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About Page end -->
+
+  <!-- Search Begin -->
+  <div class="search-model">
+    <div class="h-100 d-flex align-items-center justify-content-center">
+      <div class="search-close-switch">+</div>
+      <form class="search-model-form">
+        <input type="text" id="search-input" placeholder="Search here.....">
+      </form>
+    </div>
+  </div>
+  <!-- Search End -->
+    `
+}
